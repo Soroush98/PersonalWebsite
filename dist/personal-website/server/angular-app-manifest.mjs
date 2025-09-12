7287852f58742e27bc89c4279e75602194108070
bowrand <support@bowrand.com>
2025-09-12 14:41:09 -0600

    ..

--- dist/personal-website/server/angular-app-manifest.mjs
+++ dist/personal-website/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 12369, hash: '99053fb458eddd7b8e93acb637581dba515d600efaa04ba771c811df4a2a55b6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 12496, hash: '6e529e5aa9738e2f92ae16c967bffa0c2fef28745ea09c9faa037af2ee96ad4c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 58695, hash: '4d28751544d01feca9563822920193f8933b29fae7514dffe5907b4c5252561d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 12369, hash: '36c8fef09bea186a1a077a207ff9d0a2b47d65d88300021e228aa6cef190109b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 12496, hash: 'c0744bd912e3d993840671b68dcf68d20027b51542caf43a7b799f91120d2fa6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 58695, hash: 'b00c32342622245caabac36d8547abdf69915afb40b08f6b4afd2f9edc628a1c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-SDCDEH6E.css': {size: 663, hash: '3eB3LuSeJZU', text: () => import('./assets-chunks/styles-SDCDEH6E_css.mjs').then(m => m.default)}
   },
 };
